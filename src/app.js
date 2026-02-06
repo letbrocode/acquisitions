81d4546d9d241f5bcfae44651bca62e12f03512c
Pratik Kamble <pratikworkadd@gmail.com>
2026-02-06 20:49:36 +0530

    add logging and middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from Acquistions');
+  logger.info('Hello from Acquistions!');
+  res.status(200).send('Hello from Acquistions!');
 });
 
 export default app;
